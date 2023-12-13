f7310b98eabd5fee06b0233cb0d486ad95438374
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-12-13 15:56:52 -0300

    Api funcionando

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 from dotenv import load_dotenv
 import os
 import urllib.request, json
+import requests 
+from bs4 import BeautifulSoup
 
 def getAPIResponse(url, request):
     load_dotenv()
 
     api_url = os.getenv(url)
-    print(api_url)
     payload = request.get_json()
-    print(payload)
 
     #tratar o payload para buscar os dados e enviar o valor
 
-    headers = {'Content-Type': 'application/json'}
-    req     = urllib.request.Request(api_url, data=json.dumps(payload).encode('utf-8'), headers=headers)
+    for item in payload:
+            if 'url' in item:
+                req = requests.get(item["url"])
+                scrap = BeautifulSoup(req.text, 'html.parser')
+                valor = scrap.find_all('strong')
+                print(valor[5].text)
 
-    response = urllib.request.urlopen(req)
-    result   = response.read().decode('utf-8')
-
-    return result
+    return payload
